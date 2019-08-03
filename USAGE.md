081943ecc13943f8f57a639bd2f928c0d834b728
Santiago Martín Agra <santiagomartinagra@gmail.com>
2019-08-03 13:23:35 +0200

    WIP

---
+++ USAGE.md
@@ -0,0 +1,15 @@
+# New users to Slack
+
+## Requirements
+
+To allow the Firebase Cloud Functions to perform network request outside Google you need to enable billing.
+
+### Slack Webhook URL
+
+You need to get a the Slack Webhook URL for your desiere channel and work space, if you haven't one already.
+
+[How to get Slack Webhook URL](https://api.slack.com/incoming-webhooks)
+
+### Default User Avatar
+
+If the users doesn't have an avatar you can set a default one to see it on the Slack message :)
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,23 @@
-import * as functions from "firebase-functions";
 import { User } from "./models/User";
 import { SendUser2Slack } from "./useCase/SendUser";
 
-export const users2slack = functions.auth
-  .user()
-  .onCreate(async ({ displayName, email, photoURL, providerData }) => {
-    const user: User = {
-      name: displayName,
-      email,
-      photoURL,
-      provider: providerData[0].providerId
-    };
+export const sendNewUsers2Slack = async ({
+  displayName,
+  email,
+  photoURL,
+  providerData
+}: any) => {
+  const user: User = {
+    name: displayName,
+    email,
+    photoURL,
+    provider: providerData[0].providerId
+  };
 
-    await new SendUser2Slack({
-      defaultPhotoUrl: functions.config().mods.defaultphotourl,
-      slackWebhook: functions.config().slack.webhook
-    }).run(user);
+  await new SendUser2Slack({
+    defaultPhotoUrl: process.env.DEFAULT_AVATAR,
+    slackWebhook: process.env.SLACK_WEBHOOK as string
+  }).run(user);
 
-    return true;
-  });
+  return true;
+};
